32eff87b5e26ac3561d52ef10a43d62eafab9cd8
yhjyhjlqx <18170484646@163.com>
2025-04-20 18:47:56 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import ApkConfigForm from '../components/ApkConfigForm';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>APK Builder</h1>
+      <ApkConfigForm />
+    </div>
+  );
+}
